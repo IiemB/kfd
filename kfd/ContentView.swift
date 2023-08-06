bcf8027bf08870b5f7665b8fd8969df1ef40eb25
Iiemb <iiem.ahmad@ukur.com>
2023-08-06 08:19:14 +0700

    redisgning

--- kfd/ContentView.swift
+++ kfd/ContentView.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
+let puafPagesOptions = [16, 32, 64, 128, 256, 512, 1024, 2048]
+let puafMethodOptions = ["physpuppet", "smith"]
+let kreadMethodOptions = ["kqueue_workloop_ctl", "sem_open"]
+let kwriteMethodOptions = ["dup", "sem_open"]
+
 class Tweak: ObservableObject, Identifiable {
     var id: String
 
@@ -21,6 +26,7 @@ class Tweak: ObservableObject, Identifiable {
 class TweaksSettings: ObservableObject {
 
     @Published var tweaks: [Tweak]
+    @Published var isAnyTweakEnabled = false
 
     init() {
         self.tweaks = [
@@ -31,31 +37,57 @@ class TweaksSettings: ObservableObject {
             Tweak(valueKey: "enableCCTweaks", label: "Enable CC Custom Icons", inActiveIcon: "switch.2", activeIcon: "switch.2"),
             Tweak(valueKey: "enableLSTweaks", label: "Enable Lockscreen Custom Icons", inActiveIcon: "lock", activeIcon: "lock.fill")
         ]
+
+        self.checkIsAnyTweakEnabled(true)
     }
 
+    func checkIsAnyTweakEnabled(_ value: Bool) {
+        self.isAnyTweakEnabled = !self.tweaks.filter { $0.isEnabled }.isEmpty
+    }
+}
+
+class KFDSettings: ObservableObject {
+    @Published var puafPages: Int {
+        didSet {
+            UserDefaults.standard.set(puafPages, forKey: "puafPages")
+        }
+    }
+
+    @Published var puafMethod: Int {
+        didSet {
+            UserDefaults.standard.set(puafPages, forKey: "puafMethod")
+        }
+    }
+
+    @Published var kreadMethod: Int {
+        didSet {
+            UserDefaults.standard.set(puafPages, forKey: "kreadMethod")
+        }
+    }
+
+    @Published var kwriteMethod: Int {
+        didSet {
+            UserDefaults.standard.set(puafPages, forKey: "kwriteMethod")
+        }
+    }
+
+    init(puafPages: Int = 2048, puafMethod: Int = 1, kreadMethod: Int = 1, kwriteMethod: Int = 1) {
+        self.puafPages = UserDefaults.standard.value(forKey: "puafPages") as? Int ?? puafPages
+        self.puafMethod = UserDefaults.standard.value(forKey: "puafMethod") as? Int ?? puafMethod
+        self.kreadMethod = UserDefaults.standard.value(forKey: "kreadMethod") as? Int ?? kreadMethod
+        self.kwriteMethod = UserDefaults.standard.value(forKey: "kwriteMethod") as? Int ?? kwriteMethod
+    }
 }
 
 struct ContentView: View {
     @State private var kfd: UInt64 = 0
 
     @StateObject var tweakSettings = TweaksSettings()
-
-    @State private var puafPages = 2048
-    @State private var puafMethod = 1
-    @State private var kreadMethod = 1
-    @State private var kwriteMethod = 1
-
-    var puafPagesOptions = [16, 32, 64, 128, 256, 512, 1024, 2048]
-    var puafMethodOptions = ["physpuppet", "smith"]
-    var kreadMethodOptions = ["kqueue_workloop_ctl", "sem_open"]
-    var kwriteMethodOptions = ["dup", "sem_open"]
-
-    @State private var isSettingsPopoverPresented = false // Track the visibility of the settings popup
+    @StateObject var kfdSettings = KFDSettings()
 
     var body: some View {
         NavigationView {
             List {
-
                 if kfd != 0 {
                     Section(header: Text("Status")) {
                         VStack(alignment: .leading, spacing: 8) {
@@ -74,100 +106,86 @@ struct ContentView: View {
                     }
                 }
 
-                Button("Confirm") {
-                    kfd = do_kopen(UInt64(puafPages), UInt64(puafMethod), UInt64(kreadMethod), UInt64(kwriteMethod))
+                Section(header: Text("Payload Settings")) {
+                    Picker("puaf pages:", selection: $kfdSettings.puafPages) {
+                        ForEach(puafPagesOptions, id: \.self) { pages in
+                            Text(String(pages))
+                        }
+                    }.compositingGroup()
+                        .clipped()
+                        .pickerStyle(SegmentedPickerStyle())
+                        .disabled(kfd != 0)
 
-                    let tweaks = enabledTweaks()
+                    Picker("puaf method:", selection: $kfdSettings.puafMethod) {
+                        ForEach(0..<puafMethodOptions.count, id: \.self) { index in
+                            Text(puafMethodOptions[index])
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .disabled(kfd != 0)
+                }
 
-                    // Convert the Swift array of strings to a C-style array of char*
-                    var cTweaks: [UnsafeMutablePointer<CChar>?] = tweaks.map { strdup($0) } // Use strdup to allocate memory for C-style strings
+                Section(header: Text("Kernel Settings")) {
+                    Picker("kread method:", selection: $kfdSettings.kreadMethod) {
+                        ForEach(0..<kreadMethodOptions.count, id: \.self) { index in
+                            Text(kreadMethodOptions[index])
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .disabled(kfd != 0)
 
-                    // Add a null pointer at the end to signal the end of the array
-                    cTweaks.append(nil)
-
-                    // Pass the C-style array to do_fun along with the count of tweaks
-                    let _ = cTweaks.withUnsafeMutableBufferPointer { buffer in
-                        do_fun(buffer.baseAddress, Int32(buffer.count - 1))
-                    }
-
-                    // Deallocate the C-style strings after use to avoid memory leaks
-                    cTweaks.forEach { free($0) }
+                    Picker("kwrite method:", selection: $kfdSettings.kwriteMethod) {
+                        ForEach(0..<kwriteMethodOptions.count, id: \.self) { index in
+                            Text(kwriteMethodOptions[index])
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                        .disabled(kfd != 0)
                 }
             }
-                .navigationBarTitle("Pois0nKFD", displayMode: .inline)
-                .navigationBarItems(trailing: settingsButton)
-                .popover(isPresented: $isSettingsPopoverPresented, arrowEdge: .bottom) { settingsPopover }
+                .toolbar {
+                Button("Apply", action: enableTweaks)
+
+            }
+                .animation(.linear, value: tweakSettings.isAnyTweakEnabled)
+                .navigationBarTitle("Pois0nKFD", displayMode: .automatic)
         }
+            .environmentObject(tweakSettings)
+
+
     }
 
+    private func enableTweaks() {
 
-
-    // Settings Button in the Navigation Bar
-    private var settingsButton: some View {
-        Button(action: {
-            isSettingsPopoverPresented.toggle() // Toggle the settings popover
-        }) {
-            Image(systemName: "gearshape")
-                .imageScale(.large)
+        // Save state
+        for tweak in tweakSettings.tweaks {
+            UserDefaults.standard.setValue(tweak.isEnabled, forKey: tweak.valueKey)
         }
-    }
 
-    // Payload Settings Popover
-    private var settingsPopover: some View {
-        VStack {
-            Section(header: Text("Payload Settings")) {
-                Picker("puaf pages:", selection: $puafPages) {
-                    ForEach(puafPagesOptions, id: \.self) { pages in
-                        Text(String(pages))
-                    }
-                }.pickerStyle(SegmentedPickerStyle())
-                    .disabled(kfd != 0)
+        kfd = do_kopen(UInt64(kfdSettings.puafPages), UInt64(kfdSettings.puafMethod), UInt64(kfdSettings.kreadMethod), UInt64(kfdSettings.kwriteMethod))
 
-                Picker("puaf method:", selection: $puafMethod) {
-                    ForEach(0..<puafMethodOptions.count, id: \.self) { index in
-                        Text(puafMethodOptions[index])
-                    }
-                }.pickerStyle(SegmentedPickerStyle())
-                    .disabled(kfd != 0)
-            }
-
-            Section(header: Text("Kernel Settings")) {
-                Picker("kread method:", selection: $kreadMethod) {
-                    ForEach(0..<kreadMethodOptions.count, id: \.self) { index in
-                        Text(kreadMethodOptions[index])
-                    }
-                }.pickerStyle(SegmentedPickerStyle())
-                    .disabled(kfd != 0)
-
-                Picker("kwrite method:", selection: $kwriteMethod) {
-                    ForEach(0..<kwriteMethodOptions.count, id: \.self) { index in
-                        Text(kwriteMethodOptions[index])
-                    }
-                }.pickerStyle(SegmentedPickerStyle())
-                    .disabled(kfd != 0)
-            }
-
-            Button("Apply Settings") {
-                // Do something when the user applies the settings
-                isSettingsPopoverPresented = false // Close the popover after applying settings
-            }
-        }
-            .padding()
-    }
-
-    // Get the list of enabled tweaks based on the current state of toggles
-    private func enabledTweaks() -> [String] {
-
-        let enabledTweaks: [String] = tweakSettings.tweaks
+        // Get the list of enabled tweaks based on the current state of toggles
+        let tweaks = tweakSettings.tweaks
             .filter { $0.isEnabled }
             .map { $0.valueKey }
 
-        return enabledTweaks
+        // Convert the Swift array of strings to a C-style array of char*
+        var cTweaks: [UnsafeMutablePointer<CChar>?] = tweaks.map { strdup($0) } // Use strdup to allocate memory for C-style strings
+
+        // Add a null pointer at the end to signal the end of the array
+        cTweaks.append(nil)
+
+        // Pass the C-style array to do_fun along with the count of tweaks
+        let _ = cTweaks.withUnsafeMutableBufferPointer { buffer in
+            do_fun(buffer.baseAddress, Int32(buffer.count - 1))
+        }
+
+        // Deallocate the C-style strings after use to avoid memory leaks
+        cTweaks.forEach { free($0) }
     }
 }
 
 private struct TweakToggleView: View {
     @StateObject var tweak: Tweak
+
+    @EnvironmentObject var tweaksSetting: TweaksSettings
 
     var body: some View {
         Toggle(isOn: $tweak.isEnabled) {
@@ -176,9 +194,7 @@ private struct TweakToggleView: View {
                 Text(tweak.label)
             }
         }
-            .onChange(of: tweak.isEnabled) { newValue in
-            UserDefaults.standard.setValue(newValue, forKey: tweak.valueKey)
-        }
+            .onChange(of: tweak.isEnabled, perform: tweaksSetting.checkIsAnyTweakEnabled)
     }
 }
 
